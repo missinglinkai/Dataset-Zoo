3a33dc70391bcdf2bd680f78ca9bcb55e84fb78c
Yosi Taguri <yosi@taguri.com>
2017-12-18 14:45:48 +0200

    added dataset and new code.

--- VOC2007/create_dataset.py
+++ VOC2007/create_dataset.py
@@ -2,7 +2,6 @@ from pathlib import Path
 import argparse
 import json
 from faker import Faker
-import xml.etree.ElementTree
 from bs4 import BeautifulSoup
 
 fake = Faker()
@@ -17,7 +16,8 @@ empty_data = {'person': 0, 'bird': 0, 'cat': 0, 'cow': 0, 'dog': 0, 'horse': 0, 
 
 
 def get_file(base_path, path):
-    lines = set(map(lambda x: str(x).strip('\n'), open(str(Path(base_path).joinpath(path).absolute()), 'r')))
+    with open(str(Path(base_path).joinpath(path).absolute())) as fp:
+        lines = set(map(lambda x: str(x).strip('\n'), fp))
     return lines
 
 
@@ -32,24 +32,55 @@ def merge_dicts(*dict_args):
     return result
 
 
+test_split = set()
+train_split = set()
+val_split = set()
+trainval_split = set()
+
+split_sets = {'train': train_split, 'test': test_split, 'val': val_split, 'trainval': trainval_split}
+
+original_split_paths = Path(args.data).glob('Metadata/ImageSets/Main/*.txt')
+for split_path in original_split_paths:
+    t = get_file('', split_path)
+    datapoint_splits = map(lambda x: str(x).split(' '), t)
+
+    path_in_str = str(split_path)
+    clean_path = split_path.relative_to(args.data)
+    stem = clean_path.stem
+    stems = str(stem).split('_')
+    split = stems[len(stems) - 1]
+
+    for datapoint in datapoint_splits:
+        bucket = datapoint[0]
+        value = int(datapoint[len(datapoint)-1])
+
+        if value == 1:
+            s = split_sets[split]
+            s.add(bucket)
+
+
+
+LayoutTest = get_file(args.data, 'Metadata/ImageSets/Layout/test.txt')
 LayoutTrain = get_file(args.data, 'Metadata/ImageSets/Layout/train.txt')
 LayoutTrainVal = get_file(args.data, 'Metadata/ImageSets/Layout/trainval.txt')
 LayoutVal = get_file(args.data, 'Metadata/ImageSets/Layout/val.txt')
 
+SegmentationTest = get_file(args.data, 'Metadata/ImageSets/Segmentation/test.txt')
 SegmentationTrain = get_file(args.data, 'Metadata/ImageSets/Segmentation/train.txt')
 SegmentationTrainVal = get_file(args.data, 'Metadata/ImageSets/Segmentation/trainval.txt')
 SegmentationVal = get_file(args.data, 'Metadata/ImageSets/Segmentation/val.txt')
 
 
 def is_in_layout(bucket):
-    return bucket in LayoutTrain or bucket in LayoutTrainVal or bucket in LayoutVal
+    return bucket in LayoutTrain or bucket in LayoutTrainVal or bucket in LayoutVal or bucket in LayoutTest
 
 
 def is_in_segmentation(bucket):
-    return bucket in SegmentationTrain or bucket in SegmentationTrainVal or bucket in SegmentationVal
+    return bucket in SegmentationTrain or bucket in SegmentationTrainVal or bucket in SegmentationVal or bucket in SegmentationTest
 
 
 def enrich_layout(bucket, data):
+    data['LayoutTest'] = 1 if bucket in LayoutTest else 0
     data['LayoutTrain'] = 1 if bucket in LayoutTrain else 0
     data['LayoutTrainVal'] = 1 if bucket in LayoutTrainVal else 0
     data['LayoutVal'] = 1 if bucket in LayoutVal else 0
@@ -57,6 +88,7 @@ def enrich_layout(bucket, data):
 
 
 def enrich_segmentation(bucket, data):
+    data['SegmentationTest'] = 1 if bucket in SegmentationTest else 0
     data['SegmentationTrain'] = 1 if bucket in SegmentationTrain else 0
     data['SegmentationTrainVal'] = 1 if bucket in SegmentationTrainVal else 0
     data['SegmentationVal'] = 1 if bucket in SegmentationVal else 0
@@ -67,20 +99,27 @@ def enrich(bucket, data):
     return enrich_segmentation(bucket, enrich_layout(bucket, data)) if is_in_layout(bucket) or is_in_segmentation(
         bucket) else data
 
+def get_original_split(bucket):
+    if bucket in train_split:
+        return 'train'
+    if bucket in test_split:
+        return 'test'
+    if bucket in val_split:
+        return 'val'
 
 class_data = {}
 
 annotations = Path(args.data).glob('Annotations/*.xml')
 for path in annotations:
     with open(str(Path(path).absolute())) as fp:
-        soup = BeautifulSoup(fp)
+        soup = BeautifulSoup(fp, "html.parser")
 
     path_in_str = str(path)
     clean_path = path.relative_to(args.data)
     stem = clean_path.stem
     bucket = str(stem)
 
-    objects = soup.findAll("object")
+    objects = soup.findAll('object')
     data = dict(empty_data)
 
     for o in objects:
@@ -100,21 +139,27 @@ for path in pathlist:
 
     classes_data = dict(class_data[bucket])
 
+    original_split = get_original_split(bucket)
+
     metadataJson[str(clean_path)] = enrich(bucket, merge_dicts(classes_data, {
         'type': 'Image',
+        'OriginalSplit': original_split,
         'bucket': bucket,
     }))
 
     metadataFilename = 'Annotations/{}.xml'.format(stem)
-    metadataJson[metadataFilename] = enrich(bucket, merge_dicts(classes_data, {
-        'type': 'Annotation',
-        'bucket': bucket,
-    }))
+    if Path(args.data).joinpath(metadataFilename).exists():
+        metadataJson[metadataFilename] = enrich(bucket, merge_dicts(classes_data, {
+            'type': 'Annotation',
+            'OriginalSplit': original_split,
+            'bucket': bucket,
+        }))
 
     metadataFilename = 'SegmentationClass/{}.png'.format(stem)
     if Path(args.data).joinpath(metadataFilename).exists():
         metadataJson[metadataFilename] = enrich(bucket, merge_dicts(classes_data, {
             'type': 'SegmentationClass',
+            'OriginalSplit': original_split,
             'bucket': bucket,
         }))
 
@@ -122,6 +167,7 @@ for path in pathlist:
     if Path(args.data).joinpath(metadataFilename).exists():
         metadataJson[metadataFilename] = enrich(bucket, merge_dicts(classes_data, {
             'type': 'SegmentationObject',
+            'OriginalSplit': original_split,
             'bucket': bucket,
         }))
 
