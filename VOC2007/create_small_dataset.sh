7ebb0ab156a964e8e370ea2eb91d035dbfb23d1b
Shay Erlichmen <erlichmen@gmail.com>
2018-01-14 18:25:24 +0200

    Update create_small_dataset.sh

--- VOC2007/create_small_dataset.sh
+++ VOC2007/create_small_dataset.sh
@@ -1,6 +1,5 @@
-mkdir small_dataset
-mkdir small_dataset/Annotations
-mkdir small_dataset/JPEGImages
+mkdir -p small_dataset/Annotations
+mkdir -p small_dataset/JPEGImages
 
 #get the first 10 files from the source folder into a small_dataset
 find data/Annotations -maxdepth 1 -type f| sort |head -10 | xargs -I{} cp {} small_dataset/Annotations
